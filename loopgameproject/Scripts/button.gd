4e453829e0ac0882dda3ca30880ea3fa2981143c
TheLazMan <joshualazovic@gmail.com>
2025-08-06 20:28:11 +1000

    Added functionality to button

--- loopgameproject/Scripts/button.gd
+++ loopgameproject/Scripts/button.gd
@@ -1,7 +1,17 @@
 extends Node
 
-@onready var platform = get_node("res://Scenes/disappearing_platform.tscn")
+#@onready var platform := get_node("res://Scenes/disappearing_platform.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		platform.visible = false
+		var green_group = get_tree().get_nodes_in_group("Blocks_Green")
+		for members in green_group:
+			members.visible = false
+			members.collision = false
+		#emit_signal("button_pressed")
+
+#signal button_pressed
+#
+#var green_group = get_tree().get_nodes_in_group("Blocks_Green")
+#
+#func _on_button_button_pressed() -> void:
